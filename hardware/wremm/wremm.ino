a6dcce3ed21ec1d48249e850c32202ac75d25cb5
Maksiks <maksiksking@gmail.com>
2025-06-06 19:01:41 +0300

    feat: merged the ble and matrix code, after some trial, it works

--- hardware/wremm/wremm.ino
+++ hardware/wremm/wremm.ino
@@ -61,6 +61,63 @@ uint16_t processedData[2099];
 
 //
 
+//________________________________________________________________________________BLE part
+
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include <string>
+
+//
+// set to the built-in LED as of currently, change this if you're using this code
+#define LED_PIN 25
+
+BLECharacteristic *pCharacteristic;
+BLEServer *pServer;
+BLEAdvertising *pAdvertising;
+bool deviceConnected = false;
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+void startAdvertising() {
+  pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
+  Serial.println("Advertising started");
+}
+
+class MyCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      Serial.println("Client connected");
+    }
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+      Serial.println("Client disconnected, restarting advertising...");
+      delay(500); // surely we won't be having issues that require real throttling, right? right??
+      startAdvertising();
+    }
+};
+
+bool runImage = false;
+
+class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic) {
+    String value = pCharacteristic->getValue();
+    Serial.println("Received command: " + value);
+
+    if (value == "on") {
+      runImage = true;
+    } else if (value == "off") {
+      digitalWrite(LED_PIN, LOW);
+    }
+  }
+};
+
+//
+
 //________________________________________________________________________________VOID SETUP()
 void setup() {
   Serial.begin(115200);
@@ -109,6 +166,27 @@ void setup() {
   dma_display->clearScreen();
   delay(1000);
 
+  //
+
+  pinMode(LED_PIN, OUTPUT);
+
+  BLEDevice::init("ESP32_LED_Control");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyCallbacks());
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_WRITE
+                    );
+
+  pCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
+
+  pService->start();
+  startAdvertising();
+
+  Serial.println("Waiting for a client to connect...");
+
   // conversion loop
 
   for (size_t i = 0; i < numU16; i++) {
@@ -121,8 +199,12 @@ void setup() {
 }
 //________________________________________________________________________________
 
+int ii = 0;
+
 //________________________________________________________________________________VOID LOOP()
 void loop() {
+  Serial.println("Looping alive");
+  Serial.println(ii++);
   dma_display->setTextSize(1);    
   dma_display->setTextWrap(false);
 
@@ -170,7 +252,10 @@ void loop() {
 
   delay(1000);
 
-  dma_display->drawRGBBitmap(0, 0, processedData, 4, 4);
+  if (runImage) {
+    dma_display->drawRGBBitmap(0, 0, processedData, 4, 4);
+  }
+
 }
 //________________________________________________________________________________
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
